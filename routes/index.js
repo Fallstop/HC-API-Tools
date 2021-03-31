e7cf82e6f1574c026e3850cdc6699bfee3d22c5a
Jasper M-W <jasper@ngita.co.nz>
2021-03-31 19:53:29 +1300

    Fixed Spelling

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/gettimetableday', function(req, res, next) {
 //   res.render('index', { title: 'Hi I am + My name changes with restart! \n\n My Public IP is '+res.locals.myPublicIp+'. ENV.custom_key='+process.env.custom_key});
-	res.json({currentDay: "It worked, retrival time is " + new Date().toISOString(), cached: false})
+	res.json({currentDay: "It worked, retrieval time is " + new Date().toISOString(), cached: false})
 });
 
 
